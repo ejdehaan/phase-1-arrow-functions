9a20e5788f73048088c8cc76538f0f0453dbb577
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-03-26 22:53:18 -0400

    Passed 5 of 6 tests

---
+++ index.js
@@ -0,0 +1,12 @@
+/*const nums = [1,2,3];
+const squares = nums.map(x => x ** 2); 
+console.log(squares); //=> [1,4,9]
+console.log(nums); //=> [1,2,3]
+*/
+
+const divide = (params1, params2) => params1 / params2
+
+const square = number => number * number
+
+const add = (param1, param2) => param1 + param2
+
